98d17f6f15f90a79d3fca0a2651c83471b3cd051
David Chorvinsky <d.chorvinsky@gmail.com>
2025-07-05 09:38:22 -0400

    just destroying this

--- src/components/Film.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PhotoGallery from './PhotoGallery';
-
-const Film = () => {
-    return <PhotoGallery category="film" />;
-};
-
-export default Film;
--- src/components/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PhotoGallery from './PhotoGallery';
-
-const Home = () => {
-    return <PhotoGallery category="home" />;
-};
-
-export default Home;
